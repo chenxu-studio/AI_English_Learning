c16171dc95721f890fe43439441799b1bd5b6912
chenxu-studio <1637291937@qq.com>
2026-04-21 21:04:56 +0800

    feat: 修改注释

--- server/apps/server/src/word-book/word-book.service.ts
+++ server/apps/server/src/word-book/word-book.service.ts
@@ -14,29 +14,6 @@ export class WordBookService {
     return value === 'true' ? true : undefined;
   }
 
-  // async findAll(query: WordQuery) {
-  //   const { page = 1, pageSize = 12, word, ...rest } = query;
-  //   const tags = Object.fromEntries(
-  //     Object.entries(rest).map(([key, value]) => [key, this.toBoolean(value)]),
-  //   );
-  //   const where: Prisma.WordBookWhereInput = {
-  //     word: word ? { contains: word } : undefined,
-  //     ...tags,
-  //   };
-  //   const [total = 0, list = []] = await Promise.all([
-  //     this.prismaService.wordBook.count({ where }),
-  //     this.prismaService.wordBook.findMany({
-  //       where,
-  //       skip: (Number(page) - 1) * Number(pageSize),
-  //       take: Number(pageSize),
-  //       orderBy: {
-  //         frq: 'desc',
-  //       },
-  //     }),
-  //   ]);
-  //   return this.responseService.success({ total, list });
-  // }
-
   async findAll(query: WordQuery) {
     // 结构一下查询参数
     const { page, pageSize, word, ...rest } = query;
@@ -54,7 +31,7 @@ export class WordBookService {
         skip: (Number(page) - 1) * Number(pageSize),
         take: Number(pageSize),
         orderBy: {
-          frq: 'desc', // 按照平率从大到小排序
+          frq: 'desc', // 按照频率从大到小排序
         },
       }),
     ]);
